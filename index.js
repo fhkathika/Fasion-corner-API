b3799981bdb04b3b6cdc5ca7741250a218860855
Fariha <fhkathika057@gmail.com>
2022-10-05 15:40:17 +0600

    working on order,cart,product,users api

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const ordertRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
 dotenv.config();
 
 mongoose
@@ -17,6 +20,9 @@ app.use(express.json())
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/product", productRoute);
+app.use("/api/order", ordertRoute);
+app.use("/api/cart", cartRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
