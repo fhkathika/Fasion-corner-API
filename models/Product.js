ab71eb0e632b9461f1f5f22ab184bba77e96c2f4
Fariha <fhkathika057@gmail.com>
2022-10-11 16:38:21 +0600

    changes in Product model

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,12 @@
 const mongoose=require("mongoose");
-
-
 const ProductSchema=new mongoose.Schema(
     {
         title:{type:String,required:true,unique:true},
         desc:{type:String,require:true},
         img:{type:String,required:true},
         categories:{type:Array},
-        size:{type:String},
-        color:{type:String},
+        size:{type:Array},
+        color:{type:Array},
         price:{type:Number,required:true},
      
 
