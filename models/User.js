34998bbfc16e703c3c9a4fd6a3d13b338d750cc3
Fariha <fhkathika057@gmail.com>
2022-11-06 16:02:27 +0600

    working on login issue

--- models/User.js
+++ models/User.js
@@ -1,19 +1,28 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
 
-const userSchema=new mongoose.Schema(
+const userSchema = new mongoose.Schema(
     {
-        username:{type:String,required:true,unique:true},
-        email:{type:String,require:true,unique:true},
-        password:{type:String,required:true},
-        isAdmin:{
-            type:Boolean,
-            default:false,
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
         },
-        img:{type:String},
+        email: {
+            type: String,
+            require: true,
+            unique: true
+        },
+        password: { type: String, required: true },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        img: { type: String },
 
     },
-    {timestamps:true}
+    { timestamps: true }
 );
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
